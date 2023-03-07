3d0db9bcd2fc5555be577cf8ccbdfcd380df62bc
jobertpascua <127070522+jobertpascua@users.noreply.github.com>
2023-03-07 16:24:45 +0800

    Update streamlit_app.py
    add stop

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,21 +30,20 @@ streamlit.dataframe(fruits_to_show)
 
 
 #starttt
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
 try: 
        fruit_choice = streamlit.text_input('What fruit would you like information about?')
 # streamlit.write('The user entered ', fruit_choice)
        if not fruit_choice:
            streamlit.error("Please select a fruit to get information.")
-        else:
-              back_from_function = get_fruityvice_data(fruit_choice)
-              streamlit.dataframe(back_from_function)
-                     
-#            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-#            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-#            streamlit.dataframe(fruityvice_normalized)
+        else:                  
+            fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+            fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+            streamlit.dataframe(fruityvice_normalized)
 except URLError as e:
       streamlit.error()
+
+stream.stop()
 
 # try:
 #   fruit_choice = streamlit.text_input('What fruit would you like information about?')
@@ -57,7 +56,6 @@ except URLError as e:
 #   streamlit.error() 
 
 #endddddd
-
 
 
 
